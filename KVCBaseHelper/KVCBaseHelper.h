878e41212249098589610bb1a74b5d19c073f971
xzming <xzm@umshz.com>
2019-09-24 09:57:00 +0800

    update header

--- KVCBaseHelper/KVCBaseHelper.h
+++ KVCBaseHelper/KVCBaseHelper.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char KVCBaseHelperVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <KVCBaseHelper/PublicHeader.h>
 
-
+#import <KVCBaseHelper/CodableObject.h>
